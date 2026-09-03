52d9675b4acd9d7c5d3881f439ebfe7b789ea76d
Samuel Meijer <samuel.j.meijer@gmail.com>
2026-09-03 09:16:09 +0200

    Adding skeletons for Fragments

--- app/src/main/java/com/samuel/lifecyclev6/MainActivity2.kt
+++ app/src/main/java/com/samuel/lifecyclev6/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.samuel.lifecyclev6
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,11 +14,24 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
 
+        // TODO: Might remove, testing to see that it works as intended.
         var username = intent.getStringExtra("username");
-        var password = intent.getStringExtra("password");
-
         Log.i("SAM", "A2-username: $username")
+        var password = intent.getStringExtra("password");
         Log.i("SAM", "A2-password: $password")
+
+        val titleText = findViewById<TextView>(R.id.titleText);
+        titleText.text = "Welcome $username";
+
+        //TODO: Add components for user input
+        /*
+        Weight: TextInputEditText
+        Height: TextInputEditText => SHOW BMI
+        Date of Birth: DatePickerDialog
+        Wake up Time: TimePickerDialog
+        Favorite Color: Spinner?
+        Right or Left handed: Switch/Toggle/Radiobuttons
+         */
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
